41fe61399c94d9552caa837f3a89ee5b1f576a3e
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-12-03 12:47:26 +0600

     ৪.৪ - এলিমেন্টরে জাভাস্ক্রিপ্টের ইভেন্ট ও হুক নিয়ে কাজ করা
     ৪.৪ - এলিমেন্টরে জাভাস্ক্রিপ্টের ইভেন্ট ও হুক নিয়ে কাজ করা

--- app/public/wp-content/plugins/TimerElement/assets/js/scripts.js
+++ app/public/wp-content/plugins/TimerElement/assets/js/scripts.js
@@ -1,16 +1,9 @@
-;(function ($) {
-    $(window).on("elementor/frontend/init", function () {
-        elementorFrontend.hooks.addAction("frontend/element_ready/TimerWidget.default", function (scope, $) {
-            var clockElement = $(scope).find(".clock");
-            var displayType = clockElement.data('display-type');
-            var clockFormat = clockElement.data('clock-format');
-            if ('clock' == displayType) {
-                clockFace = '12' == clockFormat ? 'TwelveHourClock' : 'TwentyFourHourClock';
-                clockElement.FlipClock({
-                    clockFace: clockFace
-                });
-            }
+;(function ($){
+    $(window).on("elementor/frontend/init",function(){
+        // alert("Hi");
+        elementorFrontend.hooks.addAction("frontend/element_ready/TimerWidget.default", function(scope, $){
+            alert("fired");
         });
-    })
+    });
 })(jQuery);
 
--- app/public/wp-content/plugins/TimerElement/widgets/timer-widget.php
+++ app/public/wp-content/plugins/TimerElement/widgets/timer-widget.php
@@ -26,30 +26,6 @@ class Elementor_Timer_Widget extends \Elementor\Widget_Base {
 			]
 		);
 
-		/* $this->add_control( 'display_type', [
-			'label'   => __( 'Display Type', 'timerelement' ),
-			'type'    => \Elementor\Controls_Manager::SELECT,
-			'options' => [
-				'clock'    => __( 'Clock', 'timerelement' ),
-				'timerc'   => __( 'Time CountDown', 'timerelement' ),
-				'genericc' => __( 'Normal CountDown', 'timerelement' ),
-			],
-			'default' => 'clock'
-		] );
-
-		$this->add_control( 'clock_format', [
-			'label'     => __( 'Clock Format', 'timerelement' ),
-			'type'      => \Elementor\Controls_Manager::SELECT,
-			'options'   => [
-				'12' => __( '12 Hour', 'timerelement' ),
-				'24' => __( '24 Hour', 'timerelement' ),
-			],
-			'default'   => '12',
-			'condition' => [
-				'display_type' => 'clock'
-			]
-		] ); */
-
 
 		$this->end_controls_section();
 
@@ -57,14 +33,9 @@ class Elementor_Timer_Widget extends \Elementor\Widget_Base {
 
 	protected function render() {
         $settings = $this->get_settings_for_display();
-        echo "Hello";
-		/* $display_type = $this->get_settings('display_type');
-		$clock_format = $this->get_settings('clock_format'); */
+        
 		?>
-        <!-- <div class="clock"
-             data-display-type="<?php //echo $display_type ?>"
-             data-clock-format="<?php //echo $clock_format ?>"
-        ></div> -->
+        
 		<?php
 	}
 
